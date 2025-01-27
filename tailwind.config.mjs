fd3c1a325cdc70eaf7a7876d3169e17681cc2670
Jostin Aguilar <aguilarjostin2001@gmail.com>
2025-01-27 14:16:48 -0500

    add mesh gradient in header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,14 @@ export default {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "mesh-gradient":
+          "radial-gradient(circle at 50% 75%, hsla(210, 40%, 98%, 1) 0px, transparent 50%), radial-gradient(circle at 0% 0%, hsla(210, 40%, 98%, 1) 0px, transparent 40%), radial-gradient(circle at 100% 100%, hsla(210, 40%, 98%, 1) 0px, transparent 50%), radial-gradient(ellipse at 25% 0%, hsla(220, 100%, 50%, 1) 0px, transparent 50%), radial-gradient(ellipse at 75% 0%, hsla(220, 100%, 50%, 1) 0px, transparent 50%)",
+        "mesh-gradient-dark":
+          "radial-gradient(circle at 50% 75%, hsla(229, 84%, 5%, 1) 0px, transparent 50%), radial-gradient(circle at 0% 0%, hsla(229, 84%, 5%, 1) 0px, transparent 40%), radial-gradient(circle at 100% 100%, hsla(229, 84%, 5%, 1) 0px, transparent 50%), radial-gradient(ellipse at 25% 0%, hsla(220, 100%, 50%, 1) 0px, transparent 50%), radial-gradient(ellipse at 75% 0%, hsla(220, 100%, 50%, 1) 0px, transparent 50%)",
+      },
+    },
   },
   plugins: [],
 };
