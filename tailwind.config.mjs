b3bc71c68b0a66cc80f4cd4312ce910c5dafc03c
Jostin Aguilar <aguilarjostin2001@gmail.com>
2025-01-28 12:21:09 -0500

    add mesh gradient in header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,9 +6,9 @@ export default {
     extend: {
       backgroundImage: {
         "mesh-gradient":
-          "radial-gradient(circle at 50% 75%, hsla(210, 40%, 98%, 1) 0px, transparent 50%), radial-gradient(circle at 0% 0%, hsla(210, 40%, 98%, 1) 0px, transparent 40%), radial-gradient(circle at 100% 100%, hsla(210, 40%, 98%, 1) 0px, transparent 50%), radial-gradient(ellipse at 25% 0%, hsla(220, 100%, 50%, 1) 0px, transparent 50%), radial-gradient(ellipse at 75% 0%, hsla(220, 100%, 50%, 1) 0px, transparent 50%)",
+          "radial-gradient(circle at 50% 50%, hsla(210, 40%, 98%, 1) 0px, transparent 50%), radial-gradient(circle at 0% 0%, hsla(210, 40%, 98%, 1) 0px, transparent 50%), radial-gradient(circle at 100% 0%, hsla(210, 40%, 98%, 1) 0px, transparent 50%), radial-gradient(ellipse 70% 100% at 40% 0%, hsla(220, 100%, 50%, 1) 0px, transparent 50%), radial-gradient(ellipse 70% 100% at 60% 0%, hsla(250, 90%, 50%, 0.7) 0px, transparent 50%)",
         "mesh-gradient-dark":
-          "radial-gradient(circle at 50% 75%, hsla(229, 84%, 5%, 1) 0px, transparent 50%), radial-gradient(circle at 0% 0%, hsla(229, 84%, 5%, 1) 0px, transparent 40%), radial-gradient(circle at 100% 100%, hsla(229, 84%, 5%, 1) 0px, transparent 50%), radial-gradient(ellipse at 25% 0%, hsla(220, 100%, 50%, 1) 0px, transparent 50%), radial-gradient(ellipse at 75% 0%, hsla(220, 100%, 50%, 1) 0px, transparent 50%)",
+          "radial-gradient(circle at 50% 50%, hsla(229, 84%, 5%, 1) 0px, transparent 50%), radial-gradient(circle at 0% 0%, hsla(229, 84%, 5%, 1) 0px, transparent 50%), radial-gradient(circle at 100% 0%, hsla(229, 84%, 5%, 1) 0px, transparent 50%), radial-gradient(ellipse 70% 100% at 40% 0%, hsla(220, 100%, 50%, 1) 0px, transparent 50%), radial-gradient(ellipse 70% 100% at 60% 0%, hsla(250, 90%, 50%, 0.7) 0px, transparent 50%)",
       },
     },
   },
