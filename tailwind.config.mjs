f605f03db04c6ed884ceb8e4ab03756448d5f5fb
Jostin Aguilar <aguilarjostin2001@gmail.com>
2025-01-29 13:31:51 -0500

    upgrade header glow

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,7 @@ export default {
     extend: {
       backgroundImage: {
         "mesh-gradient":
-          "radial-gradient(circle at 50% 50%, hsla(210, 40%, 98%, 1) 0px, transparent 50%), radial-gradient(circle at 0% 0%, hsla(210, 40%, 98%, 1) 0px, transparent 50%), radial-gradient(circle at 100% 0%, hsla(210, 40%, 98%, 1) 0px, transparent 50%), radial-gradient(ellipse 70% 100% at 40% 0%, hsla(220, 100%, 50%, 1) 0px, transparent 50%), radial-gradient(ellipse 70% 100% at 60% 0%, hsla(250, 90%, 50%, 0.7) 0px, transparent 50%)",
+          "radial-gradient(circle at 50% 50%, hsla(210, 40%, 96%, 1) 0px, transparent 50%), radial-gradient(circle at 0% 0%, hsla(210, 40%, 96%, 1) 0px, transparent 50%), radial-gradient(circle at 100% 0%, hsla(210, 40%, 96%, 1) 0px, transparent 50%), radial-gradient(ellipse 70% 100% at 40% 0%, hsla(220, 100%, 50%, 1) 0px, transparent 50%), radial-gradient(ellipse 70% 100% at 60% 0%, hsla(250, 90%, 50%, 0.7) 0px, transparent 50%)",
         "mesh-gradient-dark":
           "radial-gradient(circle at 50% 50%, hsla(229, 84%, 5%, 1) 0px, transparent 50%), radial-gradient(circle at 0% 0%, hsla(229, 84%, 5%, 1) 0px, transparent 50%), radial-gradient(circle at 100% 0%, hsla(229, 84%, 5%, 1) 0px, transparent 50%), radial-gradient(ellipse 70% 100% at 40% 0%, hsla(220, 100%, 50%, 1) 0px, transparent 50%), radial-gradient(ellipse 70% 100% at 60% 0%, hsla(250, 90%, 50%, 0.7) 0px, transparent 50%)",
       },
