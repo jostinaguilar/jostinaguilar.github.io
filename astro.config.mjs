ff6ee89b55627ac2b14258893db0328e1510257e
Jostin Aguilar <aguilarjostin2001@gmail.com>
2025-02-04 01:09:51 -0500

    add tailwind 4 to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jostinaguilar.github.io",
-  integrations: [tailwind()],
+  integrations: [],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
